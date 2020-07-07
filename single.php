a964b40b35c9bf3e942eabdbb9feaa87e80948ca
@geo_inca <geo.inca@hotmail.com>
2020-07-07 17:17:46 +0200

    [Ticket: 0003.1] the breadcrumbs' change of position
    main border left, bottom, right
    
    	modified:   css/styles.css
    	new file:   loops/single-post.php
    	modified:   sass/site/_site.scss
    	new file:   single.php

---
+++ single.php
@@ -0,0 +1,38 @@
+<?php
+  get_header();
+  b4st_main_before();
+?>
+
+
+
+<main id="site-main"
+ class="mt-5">
+
+  <?php get_template_part('loops/single-post', get_post_format()); ?>
+
+  <?php
+  /*
+  Did you want a traditional article-plus-sidebar layout?
+  =======================================================
+  Use this below instead of the one line above -- and 
+  remove some of the CSS styles controlling `.entry-content`
+  
+  <div class="container">
+    <div class="row"> 
+      <div class="col-sm">
+        <div id="content" role="main">
+          <?php get_template_part('loops/single-post', get_post_format()); ?>
+        </div>
+      </div>
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+  */
+  ?>
+
+</main>
+
+<?php
+    b4st_main_after();
+    get_footer();
+?>
